a7b6be9f24856345e94bea5714118a7b9557ab8e
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-22 16:58:08 -0300

    :recycle: Add PromptBasedProvider protocol to AlertNotifier

--- source/notifier/AlbumAlertNotifier.js
+++ source/notifier/AlbumAlertNotifier.js
@@ -48,4 +48,12 @@ export default class AlbumAlertNotifier extends StickerAlbumNotifier {
     remainingMoneyNotEnoughDueToExcessOfRepeatedStickers(completionPercentage) {
         alert(`Well, it looks like you got so many repeat stickers that now you do not have enough money to complete the album. ${this.descriptionThatAlbumIsCompletedUpTo(completionPercentage)}`);
     }
+
+    withMoneyWillingToSpendDo(callback) {
+        callback(Number(prompt("Before we start, how much money are you willing to spend?")));
+    }
+
+    withNumberOfPacksToPurchaseDo(callback) {
+        callback(Number(prompt("How many packs do you want to purchase?")));
+    }
 }
