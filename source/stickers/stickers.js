c75371b8cb9c4bab0b6515a5118cfdbd84483dd4
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-30 01:44:44 -0300

    :recycle: Refactor stickers in new file

---
+++ source/stickers/stickers.js
@@ -0,0 +1,33 @@
+import Sticker from "./Sticker.js";
+
+const argentinaStickers = [
+    new Sticker('Argentina Logo'),
+    new Sticker('Emiliano Martinez'),
+    new Sticker('Marcos Acuña'),
+    new Sticker('Nahuel Molina'),
+    new Sticker('Nicolás Otamendi'),
+    new Sticker('Cristian Romero'),
+    new Sticker('Rodrigo de Paul'),
+    new Sticker('Ángel Di María'),
+    new Sticker('Giovanni Lo Celso'),
+    new Sticker('Leandro Paredes'),
+    new Sticker('Lautaro Martinez'),
+    new Sticker('Lionel Messi')
+]
+
+const brazilStickers = [
+    new Sticker('Brasil Logo'),
+    new Sticker('Alisson'),
+    new Sticker('Alex Sandro'),
+    new Sticker('Danilo'),
+    new Sticker('Marquinhos'),
+    new Sticker('Thiago Silva'),
+    new Sticker('Casemiro'),
+    new Sticker('Fred'),
+    new Sticker('Lucas Paquetá'),
+    new Sticker('Gabriel Jesús'),
+    new Sticker('Neymar Jr'),
+    new Sticker('Vinícius Jr')
+]
+
+export const stickers = [...argentinaStickers, ...brazilStickers];
